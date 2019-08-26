418897b619859f44cfda1160d376c60dde8ea7dd
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-08-26 01:50:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-
+#include <map>
+#include <string>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+string obtenerPais(string linea){
+    string pais;
+    for(int i=0;i<linea.size();i++){
+        if(linea[i]==' '){
+            pais=linea.substr(0,i);
+            break;
+        }
+    }
+    return pais;
+}
+
+
+int main() {
+    map<string, int> donGiovanni;
+    int n; cin>>n; cin.ignore();
+    string linea;
+    for(int i=0;i<n;i++) {
+        getline(cin, linea);
+        donGiovanni[obtenerPais(linea)]++;
+    }
+
+    for(auto p = donGiovanni.begin(); p!=donGiovanni.end();p++ ){
+        cout<<p->first<<" "<<p->second<<endl;
+    }
 }
